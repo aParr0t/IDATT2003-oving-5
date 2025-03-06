d58ec637b97838b7bd8cb952b74ca2ad813eb198
aParrot <a.lapenas04@gmail.com>
2025-03-06 11:04:55 +0100

    Refactored the scene into a separate class. Added functionality for exiting the app with the esc key

--- src/main/java/edu/ntnu/iir/bidata/Main.java
+++ src/main/java/edu/ntnu/iir/bidata/Main.java
@@ -2,18 +2,17 @@ package edu.ntnu.iir.bidata;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
   @Override
   public void start(Stage stage) {
-    String javaVersion = System.getProperty("java.version");
-    String javafxVersion = System.getProperty("javafx.version");
-    Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-    Scene scene = new Scene(new StackPane(l), 640, 480);
+    Scene scene = new Scene(new MainScene(), 640, 480);
+    scene.setOnKeyPressed(event -> {
+      switch (event.getCode()) {
+        case ESCAPE -> stage.close();
+      }
+    });
     stage.setScene(scene);
     stage.show();
   }
---
+++ src/main/java/edu/ntnu/iir/bidata/MainScene.java
@@ -0,0 +1,9 @@
+package edu.ntnu.iir.bidata;
+
+import javafx.scene.layout.StackPane;
+
+public class MainScene extends StackPane {
+  public MainScene() {
+    DeckOfCards deckOfCards = new DeckOfCards();
+  }
+}
